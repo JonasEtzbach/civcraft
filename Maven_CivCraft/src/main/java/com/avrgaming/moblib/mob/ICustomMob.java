349293dac72d6da8bec6cfbd964848bae8deac19
Jonas Etzbach <jonas.etzbach@cjt.de>
2019-12-20 15:07:09 +0100

    the conversion to a functioning maven project

---
+++ Maven_CivCraft/src/main/java/com/avrgaming/moblib/mob/ICustomMob.java
@@ -0,0 +1,35 @@
+package com.avrgaming.moblib.mob;
+
+import net.minecraft.server.v1_8_R3.DamageSource;
+import net.minecraft.server.v1_8_R3.Entity;
+import net.minecraft.server.v1_8_R3.EntityCreature;
+import net.minecraft.server.v1_8_R3.EntityLiving;
+
+public interface ICustomMob<PathfinderGoalSelector> {
+	public void onCreate();
+
+	public void onCreateAttributes();
+
+	public void onTick();
+
+	public String getBaseEntity();
+
+	public void onDamage(EntityCreature e, DamageSource source, PathfinderGoalSelector goalSelector,
+			PathfinderGoalSelector targetSelector);
+
+	public void onDeath(EntityCreature e);
+
+	public void onRangedAttack(Entity target);
+
+	public void setData(String key, String value);
+
+	public String getData(String key);
+
+	public void setEntity(EntityLiving e);
+
+	public String getSaveString();
+
+	public void loadSaveString(String str);
+
+	public String getClassName();
+}
---
+++ Maven_CivCraft/src/main/java/com/avrgaming/moblib/mob/ISpawnable.java
@@ -0,0 +1,5 @@
+package com.avrgaming.moblib.mob;
+
+public interface ISpawnable {
+	public ICustomMob getCustomMobInterface();
+}
